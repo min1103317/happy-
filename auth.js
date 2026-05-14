6a8b9ec818d4e7ef96ceedb158e30e4fe398cf68
sun <sunwoo4916@gmail.com>
2026-05-14 16:47:38 +0900

    Feature: 비밀번호 재설정 기능 추가

--- auth.js
+++ auth.js
@@ -1,4 +1,12 @@
 function login(user, pass) {
+        console.error('Invalid credentials');
+        return false;
+    }
     console.log('Logging in...');
+    return true;
+}
+
+function register(user, pass, email) {
+    console.log('Registering...');
     return true;
 }
